c22dbcf707b69e51dff0e2960c1395cd6f2d7c09
Rey Ramos Tuñacao <rey520900@gmail.com>
2021-12-11 19:30:34 +0800

    Update events.js

--- js/events.js
+++ js/events.js
@@ -3,11 +3,17 @@ $(document).ready(function() {
     $('.thanks-note').hide();
     $('.instruction-note').hide();
 
+    var link ='https://youtu.be/Tk8Zl9c49XM';
+    $('.main-event').append('<a  id="hidden-link" href="'+link+'" target="_blank">aa</a>'); 
+     
 $('.btn-event').click(function() {
     
-    window.open("https://youtu.be/Tk8Zl9c49XM", "_blank"); 
+    // window.open("https://youtu.be/Tk8Zl9c49XM", "_blank"); 
     $('.main-event').hide(); 
-    $('.instruction-note').show();
+    $('.instruction-note').show(); 
+    
+    $( "#hidden-link" ).trigger( "click" );
+    document.getElementById('hidden-link').click();
 
     setInterval(function()
     { 
@@ -18,7 +24,7 @@ $('.btn-event').click(function() {
     }, 600000 ); 
         // 1sec = 1000
         // 1min = 60000
-        // 60 min = 600000  
+        // 60 min = 600000
   });
   
 });
