7aa84a0e08eef9f1ebd8c265a1e7cb2ff3a24c2d
Rey Ramos Tuñacao <rey520900@gmail.com>
2021-12-13 01:55:16 +0800

    Update pagination.js

--- js/pagination.js
+++ js/pagination.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var max_page = 2;
     var page_main_tittles = ['KADA UMAGA | DECEMBER 10 , 2021			',
                             'A.S.P.N | DECEMBER 14 , 2021'];
-    var page_sub_tittles = ['KADA UMAGA','ASPN'];
+    var page_sub_tittles = ['A.S.P.N','KADA UMAGA'];
 
     updatePaging(current_page,max_page,page_main_tittles,page_sub_tittles)
 
